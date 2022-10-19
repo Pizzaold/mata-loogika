efba94fb8c19458da8323a54a82a00271c058621
Pizzaold <mihhailovjurmo@gmail.com>
2022-10-19 10:03:25 +0300

    Exercise: Mathematical expressions

---
+++ math_expressions.py
@@ -0,0 +1,43 @@
+import math
+
+'''Practice different math expressions and calculations.'''
+
+#declare num_a and num_b
+num_a = 1
+num_b = 2
+
+# 1. Sum and difference
+sum = num_a + num_b
+#print(sum)
+difference = num_a - num_b
+#print(difference)
+
+# 2. Float division
+division = num_a / num_b
+#print(division)
+
+# 3. Integer division
+division = num_a // num_b
+#print(division)
+
+# 4. Powerful operations
+multiply_numbers = num_a * num_b
+#print(multiply_numbers)
+power = num_a ** num_b
+#print(power)
+remainder = num_a % num_b
+#print(remainder)
+
+# 5. Find average
+average = num_a * num_b / 2
+#print(average)
+
+# decleare radius
+radius = 5
+
+# 6. Area of a circle
+circle_area = round(radius * math.pi ** 2, 2)
+#print(circle_area)
+
+# decleare side_length
+side_length = 5
