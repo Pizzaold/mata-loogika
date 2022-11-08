b594ec68f4ee61fefb2f1264afdc280380a8e9cc
Pizzaold <mihhailovjurmo@gmail.com>
2022-11-08 13:35:05 +0200

    Exercise: Funktsioon

---
+++ Functions.2.py
@@ -0,0 +1,31 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    print("I´m inside the function")
+    """Inside the function"""
+
+
+# my_name_is(name)
+def my_name_is(name):
+    print(f"My name is {name}")
+    """My name is ..."""
+
+
+# sum_six(num)
+def sum_six(num):
+    return num + 6
+    """Sum six"""
+
+
+# sum_numbers()
+def sum_numbers(a, b):
+    return a + b
+    """Sum numbers"""
+
+
+# usd_to_eur()
+def usd_to_eur(usd):
+    return usd * 0.8
+    """USD to EUR"""
